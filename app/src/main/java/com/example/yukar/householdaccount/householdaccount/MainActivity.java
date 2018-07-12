72f222f897b8b976c70da12f604019649077d0fc
welchs0614 <mimisherirai060408@gmail.com>
2018-07-13 02:42:49 +0900

    in

---
+++ app/src/main/java/com/example/yukar/householdaccount/householdaccount/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.yukar.householdaccount.householdaccount;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yukar.householdaccount.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
