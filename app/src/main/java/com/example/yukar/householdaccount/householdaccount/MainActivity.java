7b208f5a71e590423952d4ad2895231235e28c2d
welchs0614 <mimisherirai060408@gmail.com>
2018-07-13 08:51:53 +0900

    in

--- app/src/main/java/com/example/yukar/householdaccount/householdaccount/MainActivity.java
+++ app/src/main/java/com/example/yukar/householdaccount/householdaccount/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.yukar.householdaccount.householdaccount;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import com.example.yukar.householdaccount.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void btnSend_onClick(View view){
+        EditText textName=(EditText)findViewById(R.id.txtName);
+        TextView txtResult=(TextView)findViewById(R.id.txtResult);
+        txtResult.setText(String.format("%s円入力されました",txtName.getText()));
     }
 }
